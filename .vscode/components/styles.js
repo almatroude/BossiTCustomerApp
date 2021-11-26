8c9bb7dc8123505c2b7b71e883f688ac458c9b3a
Abdulai Bawa Mashud <abdulaibawam@gmail.com>
2021-11-26 23:19:10 +0000

    Signup Page

---
+++ .vscode/components/styles.js
@@ -0,0 +1,170 @@
+import styled from "styled-components";
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
+import Constants from "expo-constants";
+import { StyleSheet } from "react-native";
+
+const StatusBarHeight = Constants.statusBarHeight;
+
+//coulors
+export const Colors = {
+  primary: "#ffffff",
+  secondary: "#E5E7EB",
+  tertiary: "#1F2937",
+  darkLight: "#9CA3AF",
+  brand: "#6D28D9",
+  light: "#F1F1F1",
+  dark: "#000",
+  green: "#10B981",
+  red: "#EF4444",
+  orange: "#ffa73b",
+};
+
+const { primary, secondary, tertiary, darkLight, brand, green, red, orange } =
+  Colors;
+
+//export const backgroundColor = styled.View`
+// flex: 1,
+
+// Set content's vertical alignment.
+//  justifyContent: 'center',
+
+// Set content's horizontal alignment.
+//alignItems: 'center',
+
+// Set hex color code here.
+// backgroundColor: '${orange}',
+
+//`;
+
+export const StyledContainer = styled.View`
+  flex: 1;
+  padding: 25px;
+  padding-top: ${StatusBarHeight + 10}px;
+  background-color: ${orange};
+`;
+export const InnerContainer = styled.View`
+  flex: 1;
+  width: 100%;
+  align-items: center;
+`;
+export const PageLogo = styled.Image`
+  width: 250px;
+  height: 200px;
+`;
+export const PageTitle = styled.Text`
+  font-size: 30px;
+  text-align: center;
+  font-weight: bold;
+  color: ${brand};
+  padding: 10px;
+`;
+
+export const SubTitle = styled.Text`
+  font-size: 18px;
+  margin-bottom: 20px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: ${tertiary};
+`;
+
+export const StyledFormArea = styled.View`
+  width: 90%;
+`;
+
+export const StyledTextInput = styled.TextInput`
+  background-color: ${secondary};
+  padding: 15px;
+  padding-left: 55px;
+  padding-right: 55px;
+  border-radius: 20px;
+  font-size: 16px;
+  height: 50px;
+  margin-vertical: 3px;
+  margin-bottom: 10px;
+  color: ${tertiary};
+`;
+
+export const StyledInputLabel = styled.Text`
+  color: ${tertiary};
+  font-size: 13px;
+  text-align: left;
+`;
+
+export const LeftIcon = styled.View`
+  left: 15px;
+  top: 38px;
+  position: absolute;
+  z-index: 1;
+`;
+
+export const RightIcon = styled.TouchableOpacity`
+  right: 15px;
+  top: 38px;
+  position: absolute;
+  z-index: 1;
+`;
+
+export const StyledButton = styled.TouchableOpacity`
+  padding: 15px;
+  background-color: ${green};
+  justify-content: center;
+  align-items: center;
+  align-items: center;
+  border-radius: 5px;
+  margin-vertical: 5px;
+  height: 60px;
+  ${(props) =>
+    props.google == true &&
+    `
+    background-color: ${green};
+    flex-direction: row;
+    justify-content: center;
+  `}
+`;
+
+export const ButtonText = styled.Text`
+  color: ${primary};
+  font-size: 16px;
+  ${(props) =>
+    props.google == true &&
+    `
+    color: ${primary};
+    padding: 25px;
+  `}
+`;
+
+export const MsgBox = styled.Text`
+  text-align: center;
+  font-size: 13px;
+`;
+
+export const Line = styled.View`
+  height: 1px;
+  width: 100%;
+  background-color: ${darkLight};
+  margin-vertical: 10px;
+`;
+
+export const ExtraView = styled.View`
+  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+  padding: 10px;
+`;
+
+export const ExtraText = styled.Text`
+  justify-content: center;
+  align-content: center;
+  color: ${tertiary};
+  font-size: 15px;
+`;
+
+export const TextLink = styled.TouchableOpacity`
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TextLinkContent = styled.Text`
+  color: ${brand};
+  font-size: 15px;
+`;
---
+++ screens/Signup.js
@@ -0,0 +1,202 @@
+import React, { useState, useContext } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Formik } from "formik";
+
+// Icons
+
+import {
+  StyledContainer,
+  InnerContainer,
+  PageTitle,
+  SubTitle,
+  StyledFormArea,
+  LeftIcon,
+  StyledInputLabel,
+  StyledTextInput,
+  RightIcon,
+  StyledButton,
+  ButtonText,
+  Colors,
+  MsgBox,
+  Line,
+  ExtraView,
+  ExtraText,
+  TextLink,
+  TextLinkContent,
+  backGround,
+} from "/Users/MASHUD/Desktop/BossiTCustomerApp/.vscode/components/styles.js";
+import { View, TouchableOpacity } from "react-native";
+
+const { brand, darkLight, primary } = Colors;
+
+import { Octicons, Ionicons, Fontisto } from "@expo/vector-icons";
+
+import DateTimePicker from "@react-native-community/datetimepicker";
+
+
+
+const Signup = () => {
+  const [hidePassword, setHidePassword] = useState(true);
+  const [show, setShow] = useState(false);
+  const [date, setDate] = useState(new Date(2020, 0, 1));
+  const [dob, setDob] = useState();
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(false);
+    setDate(currentDate);
+    setDob(currentDate);
+  };
+
+  const showDatepicker = () => {
+    setShow("date");
+  };
+
+  return (
+    
+    <StyledContainer>
+      
+      <StatusBar style="dark" />
+      <InnerContainer>
+        <PageTitle>BossIt</PageTitle>
+        <SubTitle>Account Signup</SubTitle>
+        {show && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={date}
+            mode={date}
+            is24Hour={true}
+            display="default"
+            onChange={onChange}
+          />
+        )}
+        <Formik
+          initialValues={{
+            fullName: "",
+            email: "",
+            dateOfBirth: "",
+            password: "",
+            comfirmPassword: "",
+          }}
+          onSubmit={(values) => {
+            console.log(values);
+          }}
+        >
+          {({ handleChange, handleBlur, handleSubmit, values }) => (
+            <StyledFormArea>
+              <MyTextInput
+                
+                icon="person"
+                placeholder="Full Name "
+                placeholderTextColor={darkLight}
+                onChangeText={handleChange("fullName")}
+                onBlur={handleBlur("fullName")}
+                value={values.fullName}
+              />
+
+              <MyTextInput
+                
+                placeholder="Email Address"
+                placeholderTextColor={darkLight}
+                onChangeText={handleChange("email")}
+                onBlur={handleBlur("email")}
+                value={values.email}
+                keyboardType="email-address"
+                icon="mail"
+              />
+
+              <MyTextInput
+                
+                placeholder="YYYY - MM - DD"
+                placeholderTextColor={darkLight}
+                onChangeText={handleChange("dateOfBirth")}
+                onBlur={handleBlur("dateOfBirth")}
+                value={dob ? dob.toDateString() : ""}
+                icon="calendar"
+                isDate={true}
+                editable={false}
+                showDatepicker={showDatepicker}
+              />
+
+              <MyTextInput
+                
+                placeholder="Password"
+                placeholderTextColor={darkLight}
+                onChangeText={handleChange("password")}
+                onBlur={handleBlur("password")}
+                value={values.password}
+                icon="lock"
+                secureTextEntry={hidePassword}
+                isPassword={true}
+                hidePassword={hidePassword}
+                setHidePassword={setHidePassword}
+              />
+
+              <MyTextInput
+                
+                placeholder="Comfirm Password"
+                placeholderTextColor={darkLight}
+                onChangeText={handleChange("comfirmPassword")}
+                onBlur={handleBlur("comfirmPassword")}
+                value={values.comfirmPassword}
+                icon="lock"
+                secureTextEntry={hidePassword}
+                isPassword={true}
+                hidePassword={hidePassword}
+                setHidePassword={setHidePassword}
+              />
+              <MsgBox>...</MsgBox>
+              <StyledButton onPress={handleSubmit}>
+                <ButtonText>Sign up</ButtonText>
+              </StyledButton>
+              <Line></Line>
+              <ExtraView>
+                <ExtraText>Already having an account</ExtraText>
+                <TextLink>
+                  <TextLinkContent>Login</TextLinkContent>
+                </TextLink>
+              </ExtraView>
+            </StyledFormArea>
+          )}
+        </Formik>
+      </InnerContainer>
+    </StyledContainer>
+  );
+};
+
+const MyTextInput = ({
+  label,
+  icon,
+  isPassword,
+  hidePassword,
+  setHidePassword,
+  isDate,
+  showDatepicker,
+  ...props
+}) => {
+  return (
+    <View>
+      <LeftIcon>
+        <Octicons name={icon} size={30} color={brand} />
+      </LeftIcon>
+      <StyledInputLabel>{label}</StyledInputLabel>
+      {!isDate && <StyledTextInput {...props} />}
+      {isDate && (
+        <TouchableOpacity onPress={showDatepicker}>
+          <StyledTextInput {...props} />
+        </TouchableOpacity>
+      )}
+      {isPassword && (
+        <RightIcon onPress={() => setHidePassword(!hidePassword)}>
+          <Ionicons
+            name={hidePassword ? "md-eye-off" : "md-eye"}
+            size={30}
+            color={darkLight}
+          />
+        </RightIcon>
+      )}
+    </View>
+  );
+};
+
+export default Signup;
